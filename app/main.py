62c9465375d41cb7cf90e89c704a8595bbfe5a66
ModulaStudio <fatmasood01@gmail.com>
2025-05-20 13:13:01 +0300

    Initial commit 5

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 # CORS for frontend - allow access from your frontend on localhost:8080
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # Frontend allowed domain
+    allow_origins=["http://localhost:8080" ,
+                   "https://ansaar-connect-hub.vercel.app/"],  # Frontend allowed domain
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
